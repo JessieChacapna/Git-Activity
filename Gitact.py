3a6c6ff6e10c23efc8f6a772a35d25ebb05e080c
Jessie James Chacapna <jessiechacapna@gmail.com>
2024-07-10 19:34:13 +0800

    First Commit

---
+++ Gitact.py
@@ -0,0 +1,9 @@
+# Initialize the sum to 0
+total_sum = 0
+
+# Loop through numbers 1 to 10
+for number in range(1, 11):
+    total_sum += number
+
+# Print the total sum
+print("The sum of numbers from 1 to 10 is:", total_sum)
