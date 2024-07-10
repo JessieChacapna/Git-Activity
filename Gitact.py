79be3a757f2ec284142af75f3d74605b5483312e
Jessie James Chacapna <jessiechacapna@gmail.com>
2024-07-10 20:31:47 +0800

    Third commit

--- Gitact.py
+++ Gitact.py
@@ -1,9 +1,14 @@
-# Initialize the sum to 0
-total_sum = 0
+def print_pyramid(levels):
+    for i in range(1, levels + 1):
+        # Print leading spaces
+        for j in range(levels - i):
+            print(" ", end="")
+        # Print pyramid stars
+        for k in range(2 * i - 1):
+            print("*", end="")
+        # Move to the next line after each level
+        print()
 
-# Loop through numbers 1 to 10
-for number in range(1, 11):
-    total_sum += number
-
-# Print the total sum
-print("The sum of numbers from 1 to 10 is:", totalsum)
+# Specify the number of levels for the pyramid
+levels = 5
+print_pyramid(levels)
