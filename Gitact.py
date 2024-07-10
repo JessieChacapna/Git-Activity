dd00fd841ecb9a09848859a5012acfd718671420
Jessie James Chacapna <jessiechacapna@gmail.com>
2024-07-10 20:27:18 +0800

    Second Commit

--- Gitact.py
+++ Gitact.py
@@ -6,4 +6,4 @@ for number in range(1, 11):
     total_sum += number
 
 # Print the total sum
-print("The sum of numbers from 1 to 10 is:", total_sum)
+print("The sum of numbers from 1 to 10 is:", totalsum)
